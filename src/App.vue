daba4a368b80cf99387abf7f8659f99a1569ff35
Marina Gotovtseva <ceruleanblue44@gmail.com>
2023-10-04 19:19:32 +0300

    Components and props

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 
 import q from './data/quizzes.json'
 import { ref, watch } from 'vue'
+import Card from './components/Card.vue'
 
 const quizzes = ref(q)
 const search = ref('')
@@ -16,16 +17,19 @@ watch(search, () => {
   <div class="container">
     <header>
       <h1>Quizzes</h1>
-      <input 
+      <input
         v-model.trim="search"
-        type="text" 
+        type="text"
         placeholder="Search...">
     </header>
     <div class="options-container">
-
-      <div 
-        v-for="quiz in quizzes" 
-        :key="quiz.id" 
+      <Card v-for="quiz in quizzes"
+        :key="quiz.id"
+        :quiz="quiz"
+        />
+      <!-- <div
+        v-for="quiz in quizzes"
+        :key="quiz.id"
         class="card">
 
         <img :src="quiz.img" alt="">
@@ -33,7 +37,7 @@ watch(search, () => {
           <h2>{{ quiz.name }}</h2>
           <p>{{ quiz.questions.length }} questions</p>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -69,29 +73,5 @@ watch(search, () => {
     margin-top: 40px;
   }
 
-  /* CARD */
 
-  .card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
-  }
-
-  .card img {
-    width: 100%;
-    height: 190px;
-    margin: 0
-  }
-
-  .card .card-text {
-    padding: 0 5px
-  }
-
-  .card .card-text h2 {
-    font-weight: bold;
-  }
 </style>
